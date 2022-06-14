d25e5bca1ac79f1ff6a38358594090295ee7ef5a
Matheus tt.math <tt.math@disroot.org>
2022-06-13 23:07:04 -0300

    added new post time

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,5 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "trix"
 import "@rails/actiontext"
+import LocalTime from "local-time"
+LocalTime.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
+pin "local-time", to: "https://ga.jspm.io/npm:local-time@2.1.0/app/assets/javascripts/local-time.js"
