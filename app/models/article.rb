1f0a29ce1c58b6e935b0fda0ee4f8f6bf3e4d0a4
lesoname <leo.mcz@live.com>
2022-07-09 22:55:55 -0300

    recuparation files

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+    validates :title, presence: true
+    validates :content, presence: true, length: { minimum: 200 }
+    has_rich_text :content
+    extend FriendlyId
+    friendly_id :title, use: :slugged
+end
---
+++ db/migrate/20220709231425_add_slug_to_articles.rb
@@ -0,0 +1,6 @@
+class AddSlugToArticles < ActiveRecord::Migration[7.0]
+  def change
+    add_column :articles, :slug, :string
+    add_index :articles, :slug, unique: true
+  end
+end
