785a62fe8189f77f2d77b3c1d8a0fe657e0367e4
Matheus Rodrigues <96507218+ttMath@users.noreply.github.com>
2022-11-17 01:07:47 -0300

    Revert "Revert "Sign up architeture modify""

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
   root "articles#index"
   resources :articles
   get 'admins', to: 'admins#index'
