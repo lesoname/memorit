3b7eece91aa02ca5db76921067051890de03ae8e
Leonardo Mederios <lesoname@pop-os.localdomain>
2022-11-16 22:38:07 -0300

    controllers of devise generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controller: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   root "articles#index"
   resources :articles
   get 'admins', to: 'admins#index'
