bf45e34f45d5a78243901f1cde24e617286a029e
Matheus <tt.math@disroot.org>
2022-11-18 18:14:50 -0300

    register name in DB FIXED

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -53,7 +53,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # If you have extra params to permit, append them to the sanitizer.
    def configure_account_update_params
      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-   end
+   end 
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
